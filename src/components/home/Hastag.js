626355c513babaa548959a337a853e5bf569eb70
Meet Patel <meet.smarttechnica@gmail.com>
2023-06-05 15:36:22 +0530

    search all

--- src/components/home/Hastag.js
+++ src/components/home/Hastag.js
@@ -96,7 +96,7 @@ function Hastag() {
         // console.log(res.data.data);
         setTrendingsearch(res.data.data);
       })
-      .catch((err) => {});
+      .catch((err) => { });
   };
   const [popblog, setPop] = useState([]);
   const popularblog = () => {
@@ -126,7 +126,7 @@ function Hastag() {
         setCategry(response.data.data);
         // console.log(response.data.data);
       })
-      .catch((error) => {});
+      .catch((error) => { });
   };
 
   const [email, setEmail] = useState("");
@@ -148,7 +148,7 @@ function Hastag() {
         setEmail("");
         swal("Subscribed Successfully");
       })
-      .catch((error) => {});
+      .catch((error) => { });
   };
   function isValidEmail(email) {
     const expression =
@@ -261,17 +261,17 @@ function Hastag() {
               <div className="col col-lg-12 col-md-12 col-sm-12 col-xs-3">
                 {trendingsearch !== ""
                   ? trendingsearch?.slice(0, 32).map((trendingtopics) => (
-                      <button
-                        key={trendingtopics._id}
-                        onClick={() => (
-                          handlehastagtopic(trendingtopics?.topics),
-                          gaEventTracker(`${trendingtopics?.topics}`)
-                        )}
-                        className="btn1"
-                      >
-                        {trendingtopics?.topics}
-                      </button>
-                    ))
+                    <button
+                      key={trendingtopics._id}
+                      onClick={() => (
+                        handlehastagtopic(trendingtopics?.topics),
+                        gaEventTracker(`${trendingtopics?.topics}`)
+                      )}
+                      className="btn1"
+                    >
+                      {trendingtopics?.topics}
+                    </button>
+                  ))
                   : null}
               </div>
             </div>
@@ -284,12 +284,39 @@ function Hastag() {
 
       {/* warzone */}
       <Container>
-        <Row className="d-flex justify-content-center">
-          <p className="Trending">
-            <img className="mb-5" src={has1} alt="img" width="45px" />
-            Trending Warzone
-          </p>
+        <Row className="d-flex justify-content-center" style={{ alignItems: "center" }} >
+          <Col >
+            <p className="Trending">
+              <img className="mb-5" src={has1} alt="img" width="45px" />
+              Trending Warzone
+            </p>
+          </Col>
+          <Col className="text-end">
+            <Link
+              to="/category"
+              style={{ color: "black", textDecoration: "none" }}
+            > <span style={{font: "larger" }} > SEARCH All</span>
+            </Link>
+          </Col>
+
+
         </Row>
+
+        {/* <center>
+          <Link
+            to="/category"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            <Button
+              to="/category"
+              className="viewall "
+              size="lg"
+              onClick={() => gaEventTracker("VIEW All")}
+            >
+              SEARCH All
+            </Button>
+          </Link>
+        </center> */}
         <div className="trending-warzone-main position-relative">
           <Swiper
             breakpoints={{
@@ -368,9 +395,8 @@ function Hastag() {
                               className="iframesetdata"
                               width="300px"
                               style={{ borderRadius: "12px" }}
-                              src={`https://www.youtube.com/embed/${
-                                features?.resource1.link.split("v=")[1]
-                              }`}
+                              src={`https://www.youtube.com/embed/${features?.resource1.link.split("v=")[1]
+                                }`}
                             ></iframe>
                           ) : (
                             <div className="d-flex">
@@ -404,9 +430,8 @@ function Hastag() {
                             className="iframesetdata"
                             width="300px"
                             style={{ borderRadius: "12px" }}
-                            src={`https://www.youtube.com/embed/${
-                              features?.resource2.link.split("v=")[1]
-                            }`}
+                            src={`https://www.youtube.com/embed/${features?.resource2.link.split("v=")[1]
+                              }`}
                           ></iframe>
                         ) : (
                           <div className="d-flex">
@@ -607,8 +632,8 @@ function Hastag() {
             navigation
             pagination={{ clickable: true }}
             scrollbar={{ draggable: true }}
-            // onSwiper={(swiper) => console.log(swiper)}
-            // onSlideChange={() => console.log("slide change")}
+          // onSwiper={(swiper) => console.log(swiper)}
+          // onSlideChange={() => console.log("slide change")}
           >
             {feature?.map((features) => (
               <SwiperSlide key={features?._id}>
@@ -792,7 +817,7 @@ function Hastag() {
                             size={75}
                             style={{ backgroundColor: "white" }}
                             type="submit"
-                            // onClick={() => setOpenone(true)}
+                          // onClick={() => setOpenone(true)}
                           />
                         </div>
                         <div className="modalvideo">
