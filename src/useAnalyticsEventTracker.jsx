3ba920dbda41e57cd7ca382477f38ae85bc07d66
bhadresh lathiya <bhadreshl.smarttechnica@gmail.com>
2023-06-05 10:30:15 +0530

    work on leaderboard page

--- src/useAnalyticsEventTracker.jsx
+++ src/useAnalyticsEventTracker.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactGA from "react-ga";
 
 const useAnalyticsEventTracker = (category) => {
-  console.log(category, "category")
+
   const eventTracker = (action = "test action", label = "test label") => {
     ReactGA.event({ category, action, label });
   }
