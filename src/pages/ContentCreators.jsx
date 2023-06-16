f27124e4813af99d203a5d64ac64ba670267ad86
Meet Patel <meet.smarttechnica@gmail.com>
2023-06-16 11:10:17 +0530

    see more button setting

--- src/pages/ContentCreators.jsx
+++ src/pages/ContentCreators.jsx
@@ -337,10 +337,10 @@ const ContentCreators = ({ format, type, language, searchdata }) => {
 
   const handleContentClick = (event) => {
     if (event.target.checked === true) {
-      // setLimit(2);
+      setLimit(12);
       setContent("Content Creators");
     } else {
-      // setLimit(2);
+      setLimit(12);
       setContent("Content");
     }
   };
@@ -350,7 +350,7 @@ const ContentCreators = ({ format, type, language, searchdata }) => {
   };
   const handleContent = async (data) => {
     if (data === "Content") {
-      console.log(params.id, "hello")
+      // console.log(params.id, "hello")
       const responce = await axiosClient.post(
         `/user/advancefilter?sub_category=${params.id}`
       );
@@ -434,7 +434,7 @@ const ContentCreators = ({ format, type, language, searchdata }) => {
 
   useEffect(() => {
     handleContent(content);
-  }, [content, params, format, type, language, searchdata]);
+  }, [content, params, format, type, language, searchdata, limit]);
 
   useEffect(() => {
     getUser();
@@ -1515,7 +1515,7 @@ const ContentCreators = ({ format, type, language, searchdata }) => {
               fontSize: "24px",
             }}
             className="text-white fw-bold border-0 rounded-5 py-2 px-4"
-            onClick={() => seeMore(currentPage + 1)}
+            onClick={() => seeMore()}
           >
             See More
           </button>
